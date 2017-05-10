2061c51fc9306680f39e1aef50b601a59d2077f4
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2017-05-10 14:34:54 -0700

    people routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "pets", to: "pets#index", as: "pets"
-  get "pet/:id", to: "pets#show", as: "pet"
+  get "pets/:id", to: "pets#show", as: "pet"
+  post "pets", to: "pets#create"
+
+  get "people", to: "people#index", as: "people"
+  get "people/:id", to: "people#show", as: "person"
 end
