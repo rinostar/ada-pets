00b08aed8dee85b0cee2621b86a99a6009c6e38c
Kari Bancroft <kariabancroft@gmail.com>
2017-10-18 13:43:16 -0700

    remove extraneous route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "pets", to: "pets#index", as: "pets"
-  get "pet/:id", to: "pets#show", as: "pet"
+  # get "pets", to: "pets#index", as: "pets"
+  resources :pets, only: :index
 end
