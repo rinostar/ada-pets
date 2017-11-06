2ccc5ae14d61338179ba385820a7020d0a2b71d8
CheezItMan <chris@adadevelopersacademy.org>
2017-11-06 11:03:21 -0800

    finished show, create actions with tests.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,4 +7,37 @@ class PetsController < ApplicationController
       status: :ok
     )
   end
+
+  def show
+    pet = Pet.find_by(id: params[:id])
+
+    if pet
+      render json: pet.as_json(only: [:age, :id, :human, :name]), status: :ok
+    else
+      render json: { ok: false }, status: :not_found
+    end
+  end
+
+  def create
+    pet = Pet.create(pets_params)
+
+    if pet.valid?
+      render json: pet.as_json(only: [:age, :id, :human, :name]), status: :created
+    else
+      render json: {errors: pet.errors.messages}, status: :bad_request
+    end
+  end
+
+  private
+
+    def pets_params
+      params.require(:pet).permit(:human, :name, :age)
+    end
+
+
+
+
+
+
+
 end
