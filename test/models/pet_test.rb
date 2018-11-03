d8f070d6a6e2e67f9e8f5d8ca4d1a075201db035
CheezItMan <chris@adadevelopersacademy.org>
2018-11-03 12:53:37 -0700

    fixed upgrade issues

--- test/models/pet_test.rb
+++ test/models/pet_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class PetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  let (:peanut) {users(:one)}
+  it "can be created" do
+    expect(peanut.valid?).must_equal true
+  end
+
+  it "requires name, age and human" do
+    required_fields = [:name, :age, :human]
+
+    required_fields.each do |field|
+       peanut[field] = nil
+
+       expect(peanut.valid?).must_equal false
+
+       peanut.reload
+    end
+  end
 end
