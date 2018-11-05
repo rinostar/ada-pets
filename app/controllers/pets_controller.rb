676737a516864f059478bfce178c6dd7e86f80d4
CheezItMan <chris@adadevelopersacademy.org>
2018-11-05 10:20:02 -0800

    nodes api done

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,8 +6,45 @@ class PetsController < ApplicationController
     # render json: pets
     render json: pets.as_json(only: [:id, :name, :age, :human] ), status: :ok
     # render json: pets.as_json( except: [:created_at, :updated_at] )
-
   end
+
+  def show
+    pet = Pet.find_by(id: params[:id])
+
+    if pet.nil?
+      render json: { ok: false, message: 'not found' },
+          status: :not_found
+    else
+      render json: {
+        ok: true,
+        pet: pet.as_json(except: [:created_at, :updated_at])
+      }, status: :ok
+    end
+  end
+
+  def create
+    pet = Pet.new(pet_params)
+
+    if pet.save
+      render json: {
+        ok: true,
+        pet:  pet.as_json(except: [:created_at, :updated_at])
+      }, status: :ok
+    else
+      render json: {
+        ok: false,
+        message: pet.errors.messages
+      }, status: :bad_request
+
+    end
+  end
+
+
+
+
+
+
+
 
 
 
