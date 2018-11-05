40c31c26f03cd9c44efba6545ab222d7411b5d00
CheezItMan <chris@adadevelopersacademy.org>
2018-11-05 09:41:42 -0800

    group lecture

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,13 +1,19 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all
+    pets = Pet.all
+    # render json: {want_some_pie: true}
+
+    # render json: pets
+    render json: pets.as_json(only: [:id, :name, :age, :human] ), status: :ok
+    # render json: pets.as_json( except: [:created_at, :updated_at] )
+
   end
 
 
 
   private
 
-    def pet_params
-      params.require(:pet).permit(:name, :age, :human)
-    end
+  def pet_params
+    params.require(:pet).permit(:name, :age, :human)
+  end
 end
