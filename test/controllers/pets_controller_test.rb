cae0867fb2b560149d072cadb8457a6c4f2b2276
Dan Roberts <dan@adadevelopersacademy.org>
2018-11-06 10:29:53 -0800

    Positive test case for index action

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,10 +1,26 @@
 require 'test_helper'
 
-class PetsControllerTest < ActionDispatch::IntegrationTest
+describe PetsController do
+  PET_FIELDS = %w(id age name human).sort
+
   describe "index" do
     it "is a real working route" do
       get pets_path
+
       must_respond_with :success
+      # binding.pry
+      expect(response.header['Content-Type']).must_include 'json'
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Array
+      expect(body.length).must_equal Pet.count
+
+      body.each do |pet|
+        expect(pet.keys.sort).must_equal PET_FIELDS
+      end
+    end
+
+    it "returns an empty array when there are no pets" do
     end
   end
 end
