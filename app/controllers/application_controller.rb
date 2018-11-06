418acfadbee6a8c57c84e5c94f4ff18735df05c8
Dan Roberts <dan@adadevelopersacademy.org>
2018-11-05 16:37:55 -0800

    Show and create actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+
+  def render_error(status, errors)
+    render json: { errors: errors }, status: status
+  end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,16 +4,38 @@ class PetsController < ApplicationController
     # render json: {want_some_pie: true}
 
     # render json: pets
-    render json: pets.as_json(only: [:id, :name, :age, :human] ), status: :ok
+    render json: jsonify(pets), status: :ok
     # render json: pets.as_json( except: [:created_at, :updated_at] )
 
   end
 
+  def show
+    pet = Pet.find_by(id: params[:id])
+    if pet
+      render json: jsonify(pet)
+    else
+      # head :not_found
+      # render json: {}, status: :not_found
+      render_error(:not_found, { pet_id: ["no such pet"] })
+    end
+  end
 
+  def create
+    pet = Pet.new(pet_params)
+    if pet.save
+      render json: { id: pet.id }
+    else
+      render_error(:bad_request, pet.errors.messages)
+    end
+  end
 
   private
 
   def pet_params
     params.require(:pet).permit(:name, :age, :human)
+  end
+
+  def jsonify(pet_data)
+    return pet_data.as_json(only: [:id, :name, :age, :human] )
   end
 end
