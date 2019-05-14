4be59bb63d174ba9f5d4eb0ba78fcb30bd599fa4
CheezItMan <chris@adadevelopersacademy.org>
2019-05-14 10:09:36 -0700

    added index action

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,13 +1,13 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all
+    pets = Pet.all
+    render json: pets: pets.as_json(only: [:id, :name, :age, :human]), 
+           status: :ok
   end
-
-
 
   private
 
-    def pet_params
-      params.require(:pet).permit(:name, :age, :human)
-    end
+  def pet_params
+    params.require(:pet).permit(:name, :age, :human)
+  end
 end
