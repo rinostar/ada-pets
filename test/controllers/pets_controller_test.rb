b09a2823b2f4823706b5f911db00f60a2471694a
CheezItMan <chris@adadevelopersacademy.org>
2019-10-29 15:27:25 -0700

    finish index tests

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -9,5 +9,19 @@ describe PetsController do
       expect(response.header['Content-Type']).must_include 'json'
       must_respond_with :ok
     end
+
+    PET_FIELDS = ["age", "human", "name"]
+    it "responds with an array of pet hashes" do
+
+      get pets_path
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+      body.each do |pet|
+        expect(pet).must_be_instance_of Hash
+        expect(pet.keys.sort).must_equal PET_FIELDS
+      end
+    end
   end
 end
