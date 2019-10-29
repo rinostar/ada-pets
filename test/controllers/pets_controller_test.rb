6e87a467345cb6483c15669b83f1a5eb25662300
CheezItMan <chris@adadevelopersacademy.org>
2019-10-29 16:21:59 -0700

    pets controller test show action

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -23,5 +23,47 @@ describe PetsController do
         expect(pet.keys.sort).must_equal PET_FIELDS
       end
     end
+
+    it "will respond with an empty array when there are no pets" do
+      # Arrange
+      Pet.destroy_all
+
+      # Act
+      get pets_path
+      body = JSON.parse(response.body)
+
+      # Assert
+      expect(body).must_be_instance_of Array
+      expect(body).must_equal []
+    end
+  end
+
+  describe "show" do
+    it "retrieves one pet" do
+      # Arrange
+      pet = Pet.first
+
+      # Act
+      get pet_path(pet)
+      body = JSON.parse(response.body)
+
+      # Assert
+      must_respond_with :success
+      expect(response.header['Content-Type']).must_include 'json'
+      expect(body).must_be_instance_of Hash
+      expect(body.keys.sort).must_equal PET_FIELDS
+    end
+
+    it "sends back not found if the pet does not exist" do
+      # Act
+      get pet_path(-1)
+      body = JSON.parse(response.body)
+
+      # Assert
+      must_respond_with :not_found
+      expect(response.header['Content-Type']).must_include 'json'
+      expect(body).must_be_instance_of Hash
+      expect(body.keys).must_include "errors"
+    end
   end
 end
