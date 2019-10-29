e7149e007de93ef416282999f4b2f07fcfcbc71b
CheezItMan <chris@adadevelopersacademy.org>
2019-10-29 14:58:45 -0700

    initial index test

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 describe PetsController do
+
+  describe "index" do
+    it "responds with JSON and success" do
+      get pets_path
+
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :ok
+    end
+  end
 end
