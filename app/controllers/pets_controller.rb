1a667f4438794dec56e94d11e87c5c676a65eed2
CheezItMan <chris@adadevelopersacademy.org>
2019-10-29 16:22:11 -0700

    pets show action

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,6 +4,18 @@ class PetsController < ApplicationController
     render json: pets, status: :ok
   end
 
+  def show
+    pet = Pet.find_by(id: params[:id])
+
+    if pet
+      render json: pet.as_json(only: [:name, :age, :human])
+      return
+    else
+      render json: { ok: false, errors: ["Not Found"] }, status: :not_found
+      return
+    end
+  end
+
 
 
   private
