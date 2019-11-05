2601a696f969ecc3a2737c58c2887d31b7a35b9b
Rinostar <codingrinostar@gmail.com>
2019-11-05 11:03:06 -0800

    add show actions for pets cotroller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,12 +1,22 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all
+    pets = Pet.all.as_json(only: [:id, :name, :age, :human])
+    render json: pets, status: :ok
   end
 
+  def show
+    pet = Pet.find_by(id: params[:id]).as_json(only: [:id, :name, :age, :human])
 
+    if pet.nil?
+      render json: {"errors" => "not_found"}, status: :not_found
+      return
+    end
+
+    render json: pet, status: :ok
+    return
+  end
 
   private
-
     def pet_params
       params.require(:pet).permit(:name, :age, :human)
     end
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,4 +1,64 @@
 require 'test_helper'
 
 describe PetsController do
+  describe "index" do
+    it "responds with JSON and success" do
+      get pets_path
+
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :ok
+    end
+
+    it "responds with an array of pet hashes" do
+      # Act
+      get pets_path
+  
+      # Get the body of the response
+      body = JSON.parse(response.body)
+  
+      # Assert
+      expect(body).must_be_instance_of Array
+      body.each do |pet|
+        expect(pet).must_be_instance_of Hash
+        expect(pet.keys.sort).must_equal ["age", "human", "id", "name"]
+      end
+    end
+
+    it "will respond with an empty array when there are no pets" do
+      # Arrange
+      Pet.destroy_all
+  
+      # Act
+      get pets_path
+      body = JSON.parse(response.body)
+  
+      # Assert
+      expect(body).must_be_instance_of Array
+      expect(body).must_equal []
+    end
+  end
+
+  describe "show" do
+    it "retrieves one pet" do
+      pet = Pet.first
+
+      get pet_path(pet)
+      body = JSON.parse(response.body)
+      
+      must_respond_with :success
+      expect(response.header['Content-Type']).must_include 'json'
+      expect(body).must_be_instance_of Hash
+      expect(body.keys.sort).must_equal ["age", "human", "id", "name"]
+    end
+
+    it "sends back not found if the pet does not exist" do
+      get pet_path(-1)
+      body = JSON.parse(response.body)
+      
+      must_respond_with :not_found
+      expect(response.header['Content-Type']).must_include 'json'
+      expect(body).must_be_instance_of Hash
+      expect(body.keys).must_include "errors"
+    end
+  end 
 end
